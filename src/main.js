187edab2306c7375b12df2ba7eb460ec1a2e2b97
Saquib Jawed <saquibjawed4444@gmail.com>
2024-10-02 03:13:15 +0530

    Creare Contact Us, Bug Fixes

--- src/main.js
+++ src/main.js
@@ -2,5 +2,7 @@ import '../styles/modern-normalize.css';
 import '../styles/style.css';
 import '../styles/components/header.css';
 import '../styles/components/hero.css';
+import '../styles/components/about.css';
+import '../styles/components/project.css';
 import '../styles/utils.css';
 
